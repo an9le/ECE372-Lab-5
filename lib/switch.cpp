0e03b95f0e99097683931cb2fa7d95ba7185e7ce
campeestuh <165345234+campeestuh@users.noreply.github.com>
2024-04-14 14:07:15 -0700

    Update switch.cpp

--- lib/switch.cpp
+++ lib/switch.cpp
@@ -1,5 +1,16 @@
 #include <avr/io.h>
 #include "switch.h"
 
-void initSwitchPD2(){
+void initSwitch(){
+    // Using Data Direction Register to make PORTD2 an input
+    DDRD &= ~(1 << PD2); 
+
+    // Enable Pull-up resistor when using switches
+    PORTD |= (1 << PORTD2); 
+
+    // External Interrupt Mask Register
+    EIFR |= (1 << INTF2); 
+    
+    // External Interrupt Flag Register
+    EIMSK |= (1 << INT2); 
 }
