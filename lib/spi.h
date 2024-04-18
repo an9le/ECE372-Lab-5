b195cdcba736faca07c9680fb7a59588d4143438
Angel <124530206+an9le@users.noreply.github.com>
2024-04-17 21:17:46 -0700

    Update spi.h

--- lib/spi.h
+++ lib/spi.h
@@ -1,3 +1,10 @@
+// Author: Angel Perez
+// Net ID: aperez104
+// Date: 04/16/2024
+//
+// Description: 
+//----------------------------------------------------------------------//
+
 #ifndef SPI_H
 #define SPI_H
 
