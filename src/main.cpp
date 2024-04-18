3229df70dc560a152ee00c9b6877e18d58ec3d95
Alek Sepulveda <aleksepulveda911@gmail.com>
2024-04-17 23:35:08 -0700

    Small fix in main for libraries

--- src/main.cpp
+++ src/main.cpp
@@ -16,11 +16,11 @@
 
 #include <Arduino.h>
 #include <avr/io.h>
-#include "timer.h"
-#include "pwm.h"
-#include "spi.h"
-#include "switch.h"
-#include "i2c.h"
+#include <timer.h>
+#include <pwm.h>
+#include <spi.h>
+#include <switch.h>
+#include <i2c.h>
 
 // ADXL register addresses and configuration
 #define ADDRESS 0x68  
