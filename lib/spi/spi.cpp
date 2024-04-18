b5b1b0996463668a2f2f905a0aa7acf83ddcbbf6
Angel <124530206+an9le@users.noreply.github.com>
2024-04-18 12:20:44 -0700

    Update spi.cpp

--- lib/spi/spi.cpp
+++ lib/spi/spi.cpp
@@ -24,49 +24,48 @@ void initSPI() {
     // Enable SPI, MSTR, CPOL, CPHA
     // fosc / 128: SPR1 = 1, SPR0 = 1
     // ADXL345, CPOL = 1 CPHA = 1
-    SPCR |= (1 << SPIE) | (1 << SPE) | (1 << MSTR) | (1 << CPOL) | (1 << CPHA) | (1 << SPR1) | (1 << SPR0);
+    SPCR |= (1 << SPE) | (1 << MSTR) | (1 << CPOL) | (1 << CPHA) | (1 << SPR1) | (1 << SPR0);
 
-    write(0x09, 0x00);          // Decoding：Binary Coded Decimal
+    //write(0x09, 0x00);          // Decoding：Binary Coded Decimal
     write(0x0A, 0x08);          // Brightness control
     write(0x0B, 0x07);          // Scan limit: 8 LEDs
     write(0x0C, 0x01);          // Power-down mode：0，normal mode：1
     write(0x0F, 0x00);          // Test Display
-
 }
 
 
 
 void write(unsigned char command, unsigned char data) {
-    PORTB &= ~(1 << PORTB0);    // Enable chip select
+    PORTB &= ~(1 << PORTB0);    // Disable chip select
     SPDR = command;             // Load the CMD address into register
     stall;                       
     SPDR = data;                // Load the data into register
     stall;                       
-    PORTB |= (1 << PORTB0);     // Disable chip select
+    PORTB |= (1 << PORTB0);     // Enable chip select
 }
 
 
 
 void frown() {
-  write(0x01, 0b00000000);      // Row 1 LEDS
-  write(0x02, 0b01100110);      // Row 2 LEDS
-  write(0x03, 0b01100110);      // Row 3 LEDS
-  write(0x04, 0b00000000);      // Row 4 LEDS
-  write(0x05, 0b00000000);      // Row 5 LEDS
-  write(0x06, 0b00111100);      // Row 6 LEDS
-  write(0x07, 0b01000010);      // Row 7 LEDS
-  write(0x08, 0b10000001);      // Row 8 LEDS
+  write(0x01, 0b00000001);      // Row 1 LEDS
+  write(0x02, 0b00000010);      // Row 2 LEDS
+  write(0x03, 0b01100100);      // Row 3 LEDS
+  write(0x04, 0b00000100);      // Row 4 LEDS
+  write(0x05, 0b00000100);      // Row 5 LEDS
+  write(0x06, 0b01100100);      // Row 6 LEDS
+  write(0x07, 0b00000010);      // Row 7 LEDS
+  write(0x08, 0b00000001);      // Row 8 LEDS
 }
 
 
 
 void smile() { 
-  write(0x01, 0b00000000);      // Row 1 LEDS
-  write(0x02, 0b01100110);      // Row 2 LEDS
-  write(0x03, 0b01100110);      // Row 3 LEDS
-  write(0x04, 0b00000000);      // Row 4 LEDS
-  write(0x05, 0b00000000);      // Row 5 LEDS
-  write(0x06, 0b10000001);      // Row 6 LEDS
-  write(0x07, 0b01000010);      // Row 7 LEDS
-  write(0x08, 0b00111100);      // Row 8 LEDS
+  write(0x01, 0b00001000);      // Row 1 LEDS
+  write(0x02, 0b00000100);      // Row 2 LEDS
+  write(0x03, 0b01100010);      // Row 3 LEDS
+  write(0x04, 0b00000010);      // Row 4 LEDS
+  write(0x05, 0b00000010);      // Row 5 LEDS
+  write(0x06, 0b01100010);      // Row 6 LEDS
+  write(0x07, 0b00000100);      // Row 7 LEDS
+  write(0x08, 0b00001000);      // Row 8 LEDS
 }
