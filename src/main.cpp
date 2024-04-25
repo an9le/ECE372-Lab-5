8ab057327f4ff97e153c90ea171979168e32b683
Alek Sepulveda <aleksepulveda911@gmail.com>
2024-04-25 00:23:36 -0700

    Fixed issues with tilet check

--- src/main.cpp
+++ src/main.cpp
@@ -55,7 +55,7 @@ typedef enum sysStates {
 volatile face face_state = quiet_smile;
 
 // Define Check to avoid 
-bool tilt_check(int xPos, int zPos){};
+bool tilt_check(int xPos, int zPos);
 
 int main(void) {
 
@@ -140,7 +140,7 @@ bool tilt_check(int xPos, int zPos) {
     {
         return true;
     }
-    false;
+    return false;
 }
 
 // Function called when the button is pressed, i.e signal input from PORTD2
